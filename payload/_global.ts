345e6a45c41b77b639bf9dad42c0019aca015172
sokcuri <sokcuri@gmail.com>
2020-04-20 13:34:55 +0900

    update"

--- payload/_global.ts
+++ payload/_global.ts
@@ -2,8 +2,8 @@ import favicon from '../asset/favicon.ico';
 import previewImage from '../asset/preview.jpg';
 import { IGlobal } from '../component/common/IGlobal';
 
-const title = 'RESUME-NEXTJS Sample Page';
-const description = 'This is a simple web resume sample.';
+const title = 'RESUME';
+const description = 'Resume';
 
 export const _global: IGlobal.Payload = {
   favicon,
@@ -19,15 +19,15 @@ export const _global: IGlobal.Payload = {
           url: previewImage,
           width: 800,
           height: 600,
-          alt: 'OpenGraph Image Sample.png',
+          alt: '',
         },
       ],
       type: 'profile',
       profile: {
-        firstName: 'Lorem',
-        lastName: 'ipsum',
-        username: 'lorem',
-        gender: 'femail',
+        firstName: 'Jinsu',
+        lastName: 'Park',
+        username: 'sokcuri',
+        gender: 'male',
       },
     },
   },
